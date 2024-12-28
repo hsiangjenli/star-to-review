685007446422c90d4c04cc1a44290b3ea43f1fb6
Hsiang-Jen Li <hsiangjenli@gmail.com>
2024-12-28 16:07:50 +0800

    feat: Shuffle repositories and limit recheck to a specified number

--- open_reviewed_to_recheck.py
+++ open_reviewed_to_recheck.py
@@ -1,5 +1,5 @@
 import os
-
+import random
 from github import Github
 
 from core.args import parse_args
@@ -9,15 +9,20 @@ args = parse_args()
 
 token = os.getenv("GITHUB_TOKEN")
 username = args.github_username
+num_of_repos_to_recheck = 3
 
 repo_state = PydanticRepositoryState.from_json_file(filename="repo_states.json")
 github_client = Github(token)
 
 star_to_review = github_client.get_repo(f"{username}/star-to-review")
 
-for repo in repo_state.repos:
+init = 0
+for repo in random.shuffle(repo_state.repos):
+    if init >= num_of_repos_to_recheck:
+        break
     if repo.state.REVIEWED.is_active:
         star_to_review.get_issue(repo.issue_number).edit(state="open")
         repo.state.cycle()
+        init += 1
 
 repo_state.save_to_json_file(filename="repo_states.json")
